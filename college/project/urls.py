ba3d8b04c93dbaba2a132c9f1170310c47ae61f2
LMH <90649822+LoneHilke@users.noreply.github.com>
2023-01-21 15:46:16 +0100

    Har fået de værste fejl rettet
    skal lige samligne mere med original
    
    Co-Authored-By: Qvisty <18654175+qvisty@users.noreply.github.com>

--- college/project/urls.py
+++ college/project/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path
 from . import views
+from .views import UpdatePostView
 
 urlpatterns = [
     path("", views.college, name="college"),
